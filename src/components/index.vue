075088e9a9ec1192d4078f1c0e40ad52f0c71628
Aeschix <danjah@gmail.com>
2016-11-22 00:10:18 +1300

    Add more index content from original site with some links

--- src/components/index.vue
+++ src/components/index.vue
@@ -11,6 +11,22 @@
 
     <p>Become part of the rich history of adventures who have made their home in a world that is immediately fun and will continue to challenge and stimulate for years to come. Step through the portal, and immerse yourself in the mythical world, Astaria.</p>
 
+
+    <h2>New to the MUD gaming experience?</h2>
+    <p>Fairynuff. What with all of the graphic-based games out there, why would you want anything else?</p>
+
+    <h2>The Annals History</h2>
+    <p>MUDs have evolved like everything else on the internet. They began as elementary, primitive multi-user 'chat rooms' as far back as 1975, some tied in with automated D&amp;D-style Dungeon Masters, some have since been adapted into graphical games which are still widely enjoyed today. <a href="http://en.wikipedia.org/wiki/MUD" target="_blank">Wikipedia's article on MUDs</a> contains more information than any sane person should ever know.</p>
+
+    <h2>Simplicity</h2>
+    <p>There's no graphics, other than the occasional piece of ASCII art, the long list of Wizards contributing not only their invaluable time to writing the code behind the MUD, but also the rich, feature-filled content, <router-link to="/resources">just itching to be explored...</router-link></p>
+
+    <h2>It's free</h2>
+    <p>Well, Astaria is. There do exist pay-to-play MUDs, however Astaria has always, and will always be free to play.</p>
+
+    <h2>It's nich&eacute;</h2>
+    <p>Yeah, niche-aye, as in niche plus clich&eacute;. You'll probably either love or hate playing in Astaria, give it go and find out. Some of the many, many adventurers to have graced our Realm have been; Yahoo! developers, club DJ's, Radio presenters, many bored housewives, poor saps with 28k modems, naughty school kids who never get caught because their game appears study-related, known criminals, and so many others.</p>
+
   </div>
 </template>
 
