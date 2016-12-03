49a0cd91c37c34baec4d3b2e642962cda0b69e9d
Aeschix <danjah@gmail.com>
2016-12-04 00:14:53 +1300

    Separate commonly grouped content into panels for better styling.

---
+++ src/components/Panel.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="panel" :class="componentclasses">
+    <div class="panel-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<style lang="less">
+@import '../less/system-variables.less';
+.panel {
+  margin-bottom: @grid-pad * 2;
+
+  .panel-content {
+    .panel-section {
+      margin-bottom: @grid-pad;
+    }
+  }
+
+  &:last-child {
+    margin-bottom: 0px;
+  }
+}
+</style>
+<script>
+export default {
+  name: 'panel',
+
+  methods: {
+  },
+
+  props: {
+    'componentclasses': String
+  },
+
+  computed: {
+    model: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('html', value)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less">
+</style>
