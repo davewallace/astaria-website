d44ef45002b66bf40d4a1c82d530e5b465d7aefe
Dave Wallace <contact@davewallace.io>
2018-08-18 00:38:04 +1200

    Refactor MVP route and nav into abstracted components.

--- src/App.js
+++ src/App.js
@@ -15,36 +15,94 @@ import ViewMaps from './views/Maps'
 import ViewResources from './views/Resources'
 
 // For creating a main navigation, and potentially other types of navigation
-import UserNavigation from './components/davewallace.io/ui/components/UserNavigation'
+import UserNavigation from './components/ui/davewallace.io/ui/components/UserNavigation'
 
 // Style
 import 'normalize.css';
 import './style/App.css';
 
+// Main navigation definition, supplied to abstracted components
+const navMain = [
+  {
+    "name": "Home",
+    "path": "/",
+    "exact": true,
+    "component": ViewHome
+  },
+  {
+    "name": "Play",
+    "path": "/client",
+    "exact": true,
+    "component": ViewClient
+  },
+  {
+    "name": "Maps",
+    "path": "/maps",
+    "exact": true,
+    "component": ViewMaps
+  },
+  {
+    "name": "Resources",
+    "path": "/resources",
+    "exact": true,
+    "component": ViewResources
+  },
+  {
+    "name": "Gallery",
+    "path": "/gallery",
+    "exact": true,
+    "component": ViewGallery
+  }
+]
+
+// Vroom vroom!
 const App = () => (
   <Router>
     <div className="view-container">
 
       {/* Using a Switch allows us to declare our expected routes, while also
           accommodating non-matched routes (a 404 essentially) and also drop in
-          redirects as needed */}
-      <Switch>
+          redirects as needed
+
+        <Redirect from="/resources" to="/maps" />
+
         <Route exact path="/" component={ViewHome} />
         <Route path="/client" component={ViewClient} />
         <Route path="/maps" component={ViewMaps} />
         <Route path="/resources" component={ViewResources} />
         <Route path="/gallery" component={ViewGallery} />
-        {/* <Redirect from="/resources" to="/maps" /> */}
-        <Route component={ViewURLNotFound} /> {/* 404 Route */}
+
+      */}
+      <Switch>
+        {
+          navMain.map(function (item, i) {
+            return <Route exact={item.exact}
+                          path={item.path}
+                          component={item.component}
+                          key={i} />
+          })
+        }
+        {/* 404 Route */}
+        <Route component={ViewURLNotFound} />
       </Switch>
 
-      <UserNavigation test="hihi" />
+      <UserNavigation
+        classNameBlock="-main"
+        list={
+          navMain.map(function (item) {
+            return {
+              "to": item.path,
+              "name": item.name
+            }
+          })
+        }
+      />
 
     </div>
   </Router>
-);
+)
 
 /**
  * Export App entry point
  **/
-export default App;
+export default App
