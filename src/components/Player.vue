4f257449175cfde966ffa75330a759dddb94914b
Dave Wallace <contact@davewallace.io>
2019-07-13 17:58:02 +1200

    Add Astaria Player component

---
+++ src/components/Player.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="component"
+       :class="(open)? 'open' : 'closed' ">
+    [player here]
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Player',
+  props: {
+    open: false
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .component {
+    max-width: 100%;
+    height: calc(100% - 2rem);
+    min-height: 50vh;
+
+    border: 1px dashed #444;
+    padding: 1rem;
+  }
+</style>
--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="view__play">
     <h2>Step through the portal...</h2>
-    <p style="text-decoration: line-through; color: #444;">You can log in or create a new character in Astaria below...</p>
+
+    <p class="play">
+      <span>You can log in or create a new character in Astaria by selecting the icon on the right&hellip;</span>
+      <span>
+        <a href="#"
+           @click="openPlayer()">[icon]</a>
+      </span>
+      <Player :open="playerOpen" />
+    </p>
+
     <p>Or, you can download Astaria's official game program for an enhanced experience:</p>
     <ul>
       <li><a href="http://www.astariamud.com/client/A3client.exe" target="_blank" class="link">Windows (32-bit)</a></li>
@@ -11,3 +20,33 @@
     </ul>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Nav from '@/components/Player.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Player
+  },
+  created () {
+  },
+  data: function () {
+    return {
+      playerOpen: false
+    }
+  },
+  methods: {
+    openPlayer: function () {
+      this.playerOpen = !this.openPlayer;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.play {
+  padding: 20px 0;
+}
+</style>
