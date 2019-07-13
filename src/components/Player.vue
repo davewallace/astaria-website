c6cc942268dcda5877e514e77511edab8ad3e8c4
Dave Wallace <contact@davewallace.io>
2019-07-13 20:56:51 +1200

    Re-position Player component close button

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -39,8 +39,11 @@ export default {
 
   .button-close {
     position: absolute;
-    top: 10px;
-    right: 10px;
+    top: 0;
+    right: 0;
+    display: block;
+    width: 16px;
+    height: 16px;
     border: 1px dashed red;
   }
 </style>
